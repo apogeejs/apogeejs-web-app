9be949ebd4c480f5ed548e3ba18496e2e36a8920
sutter-dave <sutter@intransix.com>
2021-02-12 11:51:54 -0800

    updates to build system

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -4,7 +4,7 @@ const rollup = require('rollup');
 const replace = require('gulp-replace');
 const rename = require('gulp-rename');
 const path = require('path');
-const {getJsFileHeader, getReleaseFolder, makeSureReleaseNotPresent, createResolveAbsoluteUrl, createResolveId} = require("../../apogeejs-admin/build-src/build-utils.js");
+const buildUtils = require("../../apogeejs-admin/src/build-utils.js");
 
 const versionConfig = require('../versionConfig.json');
 
@@ -18,10 +18,10 @@ let repoName = "apogeejs-web-app";
 
 //for absolute references
 const PATH_TO_ABSOLUTE_ROOT = "../..";
-let resolveAbsoluteUrl = createResolveAbsoluteUrl(__dirname,PATH_TO_ABSOLUTE_ROOT,pathMapping);
-let resolveId = createResolveId(__dirname,PATH_TO_ABSOLUTE_ROOT,pathMapping);
+let resolveAbsoluteUrl = buildUtils.createResolveAbsoluteUrl(__dirname,PATH_TO_ABSOLUTE_ROOT,pathMapping); //used to convert urls to paths
+let resolveId = buildUtils.createResolveId(resolveAbsoluteUrl); //used for rollup
 
-const outputFolder = getReleaseFolder(repoName,version,isDevRelease);
+const outputFolder = buildUtils.getReleaseFolder(repoName,version,isDevRelease);
 
 //======================================
 // Release Info
@@ -33,14 +33,8 @@ const BASE_FILES = [
 ]
 
 let copyReleaseInfoTask = parallel(
-    () => copyFilesTask(BASE_FILES,outputFolder)
+    () => copyFiles(BASE_FILES,outputFolder)
 )
-
-/** This function is a gulp task that copies files to a destination folder. */
-function copyFilesTask(fileList,destFolder) {
-    return src(fileList,{allowEmpty: true})
-        .pipe(dest(destFolder));
-}
 
 //=================================
 // Package CSS
@@ -71,9 +65,12 @@ const CSS_FILES_URLS = [
 let cssFileSystemPaths = CSS_FILES_URLS.map(resolveAbsoluteUrl);
 
 function packageCssTask() {
-    return src(cssFileSystemPaths)
+    //fix path - related to odd problem on windows with rollup "dest"
+    let srcFiles = cssFileSystemPaths.map(buildUtils.fixPath);
+    let target = buildUtils.fixPath(outputFolder);
+    return src(srcFiles)
         .pipe(concat(CSS_BUNDLE_FILENAME))
-        .pipe(dest(outputFolder))
+        .pipe(dest(target))
 }
 
 
@@ -89,8 +86,11 @@ const RESOURCE_URL_PATTERN = "/apogeejs-admin/resources/**/*";
 let resourceSystemFilesPattern = resolveAbsoluteUrl(RESOURCE_URL_PATTERN);
 
 function copyResourcesTask() {
-    return src(resourceSystemFilesPattern)
-        .pipe(dest(path.join(outputFolder,RESOURCES_FOLDER_NAME)))
+    //fix path - related to odd problem on windows with rollup "dest"
+    let srcPattern = buildUtils.fixPath(resourceSystemFilesPattern);
+    let target = buildUtils.fixPath(path.join(outputFolder,RESOURCES_FOLDER_NAME))
+    return src(srcPattern)
+        .pipe(dest(target))
 }
 
 //----------------
@@ -104,8 +104,12 @@ const ACE_INCLUDE_URL_PATTERN = "/apogeejs-admin/ext/ace/ace_1.4.3/ace_includes/
 let aceIncludeSystemFilesPattern = resolveAbsoluteUrl(ACE_INCLUDE_URL_PATTERN);
 
 function copyAceIncludesTask() {
-    return src(aceIncludeSystemFilesPattern)
-        .pipe(dest(path.join(outputFolder,ACE_INCLUDES_FOLDER_NAME)))
+    //fix path - related to odd problem on windows with rollup "dest"
+    let srcPattern = buildUtils.fixPath(aceIncludeSystemFilesPattern);
+    let target = buildUtils.fixPath(path.join(outputFolder,ACE_INCLUDES_FOLDER_NAME))
+
+    return src(srcPattern)
+        .pipe(dest(target))
 }
 
 //----------------
@@ -120,12 +124,13 @@ const GLOBAL_SRC_URLS = [
 //convert for remapped directories
 let globalSrcFileSystemPaths = GLOBAL_SRC_URLS.map(resolveAbsoluteUrl);
 
-let copyGlobalFiles = () => copyFilesTask(globalSrcFileSystemPaths,outputFolder)
+let copyGlobalFiles = () => copyFiles(globalSrcFileSystemPaths,outputFolder)
 
 //==============================
 // Web App
 //==============================
 
+const SRC_WEB_PAGE_FILE = '../src/apogee.DEPLOY.html'
 const WEB_PAGE_NAME = "apogee.html"
 const WEB_APP_JS_FILENAME = "apogeeWebApp.js";
 
@@ -137,11 +142,15 @@ let releaseWebAppTask = parallel(
 function copyWebAppPageTask() {
     let baseHref = versionAssetsPath + "/";
 
-    return src('../src/apogee.DEPLOY.html')
+    //fix path - related to odd problem on windows with rollup "dest"
+    let srcFile = buildUtils.fixPath(SRC_WEB_PAGE_FILE);
+    let target = buildUtils.fixPath(outputFolder);
+
+    return src(srcFile)
         .pipe(replace("BASE_HREF",baseHref))
         .pipe(replace("APOGEE_VERSION",version))
         .pipe(rename(WEB_PAGE_NAME))
-        .pipe(dest(outputFolder));
+        .pipe(dest(target));
 }
 
 function packageWebAppTask() {
@@ -155,10 +164,26 @@ function packageWebAppTask() {
             { 
                 file: path.join(outputFolder,WEB_APP_JS_FILENAME),
                 format: 'es',
-                banner: getJsFileHeader(WEB_APP_JS_FILENAME,version)
+                banner: buildUtils.getJsFileHeader(WEB_APP_JS_FILENAME,version)
             }
         );
     });
+}
+
+//=============================
+// internal functions
+//=============================
+
+/** This function is a gulp task that copies files to a destination folder. */
+function copyFiles(fileList,destFolder) {
+    //I had some occasional problems on windows without this step
+    //I think this is related to an issue cited in the rollup "dest" documentation, but
+    //I didn't understard how to otherwise fix it.
+    let alteredDestFolder = buildUtils.fixPath(destFolder);
+    let alteredFileList = fileList.map(buildUtils.fixPath);
+
+    return src(alteredFileList,{allowEmpty: true})
+        .pipe(dest(alteredDestFolder));
 }
 
 //============================
@@ -167,7 +192,7 @@ function packageWebAppTask() {
 
 //This task executes the complete release
 exports.release = series(
-    () => makeSureReleaseNotPresent(outputFolder),
+    () => buildUtils.makeSureReleaseNotPresent(outputFolder),
     parallel(
         copyReleaseInfoTask,
         packageCssTask,
